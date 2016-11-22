93af473eab08c9dd50f19d69746bf25ef31576c7
Gabriel Ferraz <gferraz@fullsail.com>
2016-11-22 13:39:56 -0500

    adds browser sync and miify html to browserify-babelify-single-file

--- gulp-practice-browserify-babelify-single-file/gulpfile.js
+++ gulp-practice-browserify-babelify-single-file/gulpfile.js
@@ -1,7 +1,20 @@
+// check this repo: https://github.com/babel/babelify#options
+// check this forum: https://github.com/babel/babelify/issues/157
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
+var browserSync = require('browser-sync');
+
+// server config
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist' // serves index.html from /dist
+    },
+  });
+});
+
 //no need to import if we are not using the plugin directly, 
 //but we need to have the node module if we are going to use the option "babelify"
 //in the browserify transform like we do here
@@ -11,8 +24,8 @@ var plugins = require('gulp-load-plugins')();
 
 // the best way to use browserify is to import all js into a 'main.js' and use that file as the source for browserify
 // use this if for any reason we cannot import all files to a 'main.js' and browserify that file only
-gulp.task('scripts', function(done) {
-  browserify('src/main.js') // if we need to add more files we can pass something like ['src/main.js', 'src/first.js', 'src/second.js'] into 'browserify'
+gulp.task('scripts', function() {
+  browserify({entries: 'src/js/main.js', debug: true}) // if we need to add more files we can pass something like ['src/main.js', 'src/first.js', 'src/second.js'] into 'browserify'
   // alternatively to the transform below we can add the following config in package.json
   // "browserify": {"transform": [["babelify", { "presets": ["es2015"] }]]}
   // and simply pass babelify, the plugin itself (no quotes) to the transform
@@ -22,16 +35,24 @@ gulp.task('scripts', function(done) {
   .pipe(source('script.min.js')) // in memory bundled file
   .pipe(buffer()) // need to buffer to be able to use other js plugins
   .pipe(plugins.uglify())
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.reload({ stream: true }))
 });
 
-
-// run task "scripts" then start watching files in /src 
-// and run task scripts if they change
-// run default task before watch task
-gulp.task('watch', ['scripts'], function() {
-  gulp.watch('src/*.js', ['scripts'])
+// minify html
+gulp.task('minifyHtml', function() {
+  gulp.src('src/index.html')
+    .pipe(plugins.htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({ stream: true }))
 });
+
+// order of tasks matter here: 'minifyHtml' has to be executed first for browser sync to work properly
+gulp.task('watch', ['minifyHtml', 'scripts', 'browserSync'], function() {
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/index.html', ['minifyHtml']);
+});
+
 
 gulp.task('default', ['watch']);
 
